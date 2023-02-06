8b50878234f0ddfb89f48f82c69eafdd19c30e93
Hyang6450 <s1422@naver.com>
2023-02-06 23:17:22 +0900

    시립도서관 자료검색

--- library/src/main/java/com/korit/library/entity/SearchBook.java
+++ library/src/main/java/com/korit/library/entity/SearchBook.java
@@ -1,7 +1,6 @@
 package com.korit.library.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Builder
 public class SearchBook {
     private int bookId;
     private String bookCode;
--- library/src/main/java/com/korit/library/web/api/SearchApi.java
+++ library/src/main/java/com/korit/library/web/api/SearchApi.java
@@ -1,20 +1,32 @@
 package com.korit.library.web.api;
 
+import com.korit.library.service.SearchService;
 import com.korit.library.web.dto.CMRespDto;
 import com.korit.library.web.dto.SearchBookReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class SearchApi {
 
+    private final SearchService searchService;
+
     @GetMapping("/search")
     public ResponseEntity<CMRespDto<?>> search(SearchBookReqDto searchBookReqDto) {
+
         return ResponseEntity.ok()
-                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", null));
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", searchService.getSearchBooks(searchBookReqDto)));
+    }
+
+    @GetMapping("/search/totalcount")
+    public ResponseEntity<CMRespDto<Integer>> getSearchBookTotalCount(SearchBookReqDto searchBookReqDto) {
+        return ResponseEntity.ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", searchService.getSearchTotalCount(searchBookReqDto)));
     }
 }
